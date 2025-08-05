14eb4d780671cc669fa3a0fa5d3a37992b2d832a
jpjorgesz <joao.pedro.jorge@escola.pr.gov.br>
2025-08-05 07:56:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const botaoMostraPalavras = document. querySelector('#botao-palavrachave');
+
+botaoMostraPalavras.addEventListener('click',botaomostraPlavraChave);
+
+function botaomostraPlavraChave(){
+    alert('fui clicado!');
+}
